0f27b8ca4b4508cbb22b119b759c3b569e06a4a1
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2024-11-26 20:20:40 -0500

    add findMean function

--- helpers.js
+++ helpers.js
@@ -50,6 +50,15 @@ function convertAndValidateNumsArray(numsAsStrings) {
   return result;
 }
 
+function findMean(nums) {
+  if (nums.length === 0) return 0;
+
+  return (
+    nums.reduce(function (acc, cur) {
+      return acc + cur;
+    }) / nums.length
+  );
+}
 module.exports = {
   findMode,
   convertAndValidateNumsArray,
