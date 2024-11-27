8e59bf7c2443dced35b1396ca5078886f0021a02
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2024-11-26 20:05:42 -0500

    add app.get('/mode') with error handling

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ app.get("/", (req, res) => {
   return res.send("Homepage");
 });
 
+app.get("/mode", (req, res, next) => {
+  if (!req.query.nums) {
+    throw new ExpressError(
+      `You must pass a query key of nums with a comma-seperated list of numbers.`,
+      400
+    );
+  }
+  let numsAsStrings = req.query.nums.split(",");
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums),
+  };
+
+  return res.send(result);
+});
 app.listen(3000, function () {
   console.log("App on port 3000");
 });
