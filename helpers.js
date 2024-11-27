32cc1bf369999788afd4f55658b0b5d6dc5471a0
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2024-11-26 19:15:10 -0500

    add 'findMode' function

--- helpers.js
+++ helpers.js
@@ -8,5 +8,20 @@ function createFrequencyCounter(arr) {
     acc[next] = (acc[next] || 0) + 1;
 
     return acc;
-  });
+  }, {});
+}
+
+function findMode(arr) {
+  let freqCounter = createFrequencyCounter(arr);
+
+  let count = 0;
+  let mostFrequent;
+
+  for (let key in freqCounter) {
+    if (freqCounter[key] > count) {
+      mostFrequent = key;
+
+      count = freqCounter[key];
+    }
+  }
 }
